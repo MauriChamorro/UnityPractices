64093dbe85b42aeaa3d91193e7ee1d57cb9a3332
Mauri Chamorro <mmchamoo@gmail.com>
2019-03-14 22:10:32 -0300

    initss

---
+++ Assets/Data/Scripts/InventoryItemList.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InventoryItemList", menuName = "Inventory/ItemList")]
+public class InventoryItemList : ScriptableObject
+{
+    public List<InventoryItem> itemList;
+}
---
+++ Assets/Data/Scripts/MyScriptableObjectClass.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "DataContainer", menuName = "Inventory/List", order = 1)]
+public class MyScriptableObjectClass : ScriptableObject
+{
+    public string objectName = "Luces";
+    public bool colorIsRandom = false;
+    public Color thisColor = Color.white;
+    public Vector3[] spawnPoints;
+}
